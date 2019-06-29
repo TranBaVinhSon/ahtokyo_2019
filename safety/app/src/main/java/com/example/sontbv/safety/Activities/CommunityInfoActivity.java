5606754120829d3c7de131bd022307bc9802fee9
Tran B.V. Son <tranbavinhson@gmail.com>
2019-06-29 23:24:38 +0900

    add stuff

--- safety/app/src/main/java/com/example/sontbv/safety/Activities/CommunityInfoActivity.java
+++ safety/app/src/main/java/com/example/sontbv/safety/Activities/CommunityInfoActivity.java
@@ -1,6 +1,7 @@
 package com.example.sontbv.safety.Activities;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,8 +30,8 @@ public class CommunityInfoActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent myIntent = new Intent(CommunityInfoActivity.this, CommunityActivity.class);
+                CommunityInfoActivity.this.startActivity(myIntent);
             }
         });
     }
